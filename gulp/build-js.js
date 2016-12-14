a0dca322653460082609004aa184bdcd73a6fe6b
wenhaoquantil <wenhao.lin@quantil.com>
2016-12-13 23:56:39 -0800

    create gulp tasks

--- gulp/build-js.js
+++ gulp/build-js.js
@@ -1,12 +1,43 @@
-var gulp= require("gulp");
-var browserify = require('gulp-browserify');
+var gulp = require("gulp");
+var browserify = require("browserify");
+var gulpBrowserify  = require('gulp-browserify');
+var source = require('vinyl-source-stream');
+var transform = require('vinyl-transform');
 
-gulp.task("build-js", function() {
-    gulp.src('./client/**/*-page.js')
-        .pipe(browserify({
-            insertGlobals : true,
-            debug : false
-        }))
-        .pipe(gulp.dest('./public/javascripts'))
+
+const vendors = ['jquery', 'angular', 'angular-resource', 'angular-route'];
+
+
+gulp.task('build:vendor', function () {
+    const b = browserify({
+        debug: false
+    });
+
+    vendors.forEach(function (lib) {
+        b.require(lib);
+    });
+
+    b.bundle()
+        .pipe(gulp.src('vendor.js'))
+        .pipe(gulp.dest('./public/javascripts'));
 });
 
+
+gulp.task("build:app", function () {
+    gulp.src('./client/**/*-page.js')
+        .pipe( gulpBrowserify ({
+            insertGlobals: true,
+            debug: false
+        }))
+        .on('prebundle', function (bundle) {
+                vendors.forEach(function (lib) {
+                    bundle.external(lib);
+                })
+            })
+        .pipe(gulp.dest('./public/javascripts'));
+});
+
+
+gulp.task("build-js", ["build:vendor"], function () {
+    console.log("build-js is done")
+});
