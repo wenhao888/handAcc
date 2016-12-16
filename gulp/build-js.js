128270fefcf10902d965f2de806a69d44d85dd50
wenhaoquantil <wenhao.lin@quantil.com>
2016-12-15 22:50:11 -0800

    use browserify-shim to jquery and angularjs

--- gulp/build-js.js
+++ gulp/build-js.js
@@ -1,39 +1,13 @@
 var gulp = require("gulp");
 var browserify  = require('gulp-browserify');
 
-const vendors = ['jquery', 'angular', 'angular-resource', 'angular-route'];
 
-
-gulp.task('build:vendor', function () {
-    gulp.src('./client/vendor.js',{ read: false })
-        .pipe( browserify ({
-            insertGlobals: false,
-            debug: false
-        }))
-        .on('prebundle', function (bundle) {
-            vendors.forEach(function (lib) {
-                bundle.require(lib, {expose: lib});
-            })
-        })
-        .pipe(gulp.dest('./public/javascripts'));
-});
-
-
-gulp.task("build:app", function () {
+gulp.task("build-js", function () {
     gulp.src('./client/**/*-page.js', { read: false })
         .pipe( browserify ({
-            insertGlobals: false,
+            insertGlobals: true,
             debug: false
         }))
-        .on('prebundle', function (bundle) {
-                vendors.forEach(function (lib) {
-                    bundle.external(lib);
-                })
-            })
         .pipe(gulp.dest('./public/javascripts'));
 });
 
-
-gulp.task("build-js", ["build:vendor","build:app"], function () {
-    console.log("build-js is done")
-});
