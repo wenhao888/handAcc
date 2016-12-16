56d3275580a644a5e7348478cf77f281a853a65f
wenhaoquantil <wenhao.lin@quantil.com>
2016-12-16 00:41:04 -0800

    change to use ui-bootstrap

--- client/home/home-page.js
+++ client/home/home-page.js
@@ -1,9 +1,26 @@
-var angular=require("angular");
+//var angular=require("angular");
 
-var home = angular.module("home",[]);
+var home = angular.module("home",["ui.bootstrap"]);
 
 home.controller("homeController", ["$scope", function($scope) {
-    $scope.info="hello";
+    $scope.slides= [
+        {   id: 1,
+            src:"/public/images/image1.jpg"
+        },
+        {   id: 2,
+            src:"/public/images/image2.jpg"
+        },
+        {   id: 3,
+            src:"/public/images/image3.jpg"
+        },
+        {   id: 4,
+            src:"/public/images/image4.jpg"
+        }
+    ];
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    $scope.message = "hello";
+
 }]);
 
-console.log("home is initialized");
