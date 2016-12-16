b9211130d10e470cb1a6fe6ed66cb41bf367eb92
wenhaoquantil <wenhao.lin@quantil.com>
2016-12-16 00:13:18 -0800

    change to use ui-bootstrap

--- client/home/home-page.js
+++ client/home/home-page.js
@@ -1,10 +1,7 @@
-require("angular-touch");
-require("angular-carousel");
 var angular=require("angular");
 
+var home = angular.module("home",[]);
 
-
-var home = angular.module("home",['angular-carousel']);
 home.controller("homeController", ["$scope", function($scope) {
     $scope.info="hello";
 }]);
