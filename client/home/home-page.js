d062a15d478401c6ad9d9b52e7f078f3aca88743
wenhaoquantil <wenhao.lin@quantil.com>
2016-12-15 23:33:55 -0800

    start to work on carousel

--- client/home/home-page.js
+++ client/home/home-page.js
@@ -1,5 +1,12 @@
-
+require("angular-touch");
+require("angular-carousel");
 var angular=require("angular");
-var home = angular.module("home",[]);
+
+
+
+var home = angular.module("home",['angular-carousel']);
+home.controller("homeController", ["$scope", function($scope) {
+    $scope.info="hello";
+}]);
 
 console.log("home is initialized");
