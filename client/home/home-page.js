67b2869af073daff68c10c6df8a4805894781e9f
wenhaoquantil <wenhao.lin@quantil.com>
2016-12-16 00:57:55 -0800

    change to use ui-bootstrap

--- client/home/home-page.js
+++ client/home/home-page.js
@@ -5,22 +5,21 @@ var home = angular.module("home",["ui.bootstrap"]);
 home.controller("homeController", ["$scope", function($scope) {
     $scope.slides= [
         {   id: 1,
-            src:"/public/images/image1.jpg"
+            src:"/images/image1.jpg"
         },
         {   id: 2,
-            src:"/public/images/image2.jpg"
+            src:"/images/image2.jpg"
         },
         {   id: 3,
-            src:"/public/images/image3.jpg"
+            src:"/images/image3.jpg"
         },
         {   id: 4,
-            src:"/public/images/image4.jpg"
+            src:"/images/image4.jpg"
         }
     ];
     $scope.myInterval = 5000;
-    $scope.noWrapSlides = false;
-    $scope.active = 0;
-    $scope.message = "hello";
+    $scope.noWrapSlides = true;
+    $scope.active = 1;
 
 }]);
 
--- server/app.js
+++ server/app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-const expressNunjucks = require('express-nunjucks');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -13,11 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-const njk = expressNunjucks(app, {
-  watch: true,
-  noCache: true
-});
-
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
