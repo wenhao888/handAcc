bf12713385661df37a1773d14dbb8d54cc2d9b2c
wenhaoquantil <wenhao.lin@quantil.com>
2016-12-30 11:25:51 -0800

    adjust routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var home = require("./home");
+var user = require("./user");
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use("/", home);
+router.use("/user", user);
 
 module.exports = router;
