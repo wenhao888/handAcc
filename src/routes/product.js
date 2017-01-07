bcc137700f5f2f7a5a4e7cdee50a1b1ffb3249b8
wenhao <wenhao.lin@gmail.com>
2017-01-07 10:24:39 -0800

    Add user issue page

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var productService = require("../service/product/productService");
 
 /* GET users listing. */
 router.get('/list', function(req, res, next) {
@@ -11,6 +10,10 @@ router.get('/:id/detail', function(req,res,next) {
     var id = req.params.id;
     var product = productService.getProduct(id);
     res.render("product/productDetail", {layout:'layout/general', "product": product})
+});
+
+router.get('/download', function(req, res, next) {
+    res.render('product/download', {layout:'layout/general'});
 });
 
 module.exports = router;
