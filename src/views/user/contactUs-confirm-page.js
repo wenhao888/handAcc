65174280a2e8108017a7577940a94c594e5ae918
wenhaoquantil <wenhao.lin@quantil.com>
2017-01-22 16:16:08 -0800

    improve close in contactUs-confirm page

--- src/views/user/contactUs-confirm-page.js
+++ src/views/user/contactUs-confirm-page.js
@@ -5,6 +5,12 @@ var contactConfirm= angular.module("contactConfirm",[]);
 
 contactConfirm.controller("contactConfirmController",["$scope", function($scope) {
     $scope.close = function() {
-        window.history.go(-2);
+        if (typeof document.referrer !='undefined' && document.referrer) {
+            //if come from the contactUs page
+            window.history.go(-2)
+        } else {
+            //else, for example user enter the url directly
+            window.history.go(-1);
+        }
     }
 }]);
