70ee0034eb9ca7621676a0666eadc721b5ace117
wenhaoquantil <wenhao.lin@quantil.com>
2017-01-24 23:29:11 -0800

    save before change

---
+++ src/views/user/issue-page.js
@@ -0,0 +1,10 @@
+var angular=require("angular");
+var issue=angular.module("issue",[]);
+
+issue.controller("issueController",["$scope", function ($scope) {
+    $scope.priority="normal";
+
+
+
+
+}]);
