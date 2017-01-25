c8f8b3d2204dfb812dff56efb9a3a425fa7fc307
wenhaoquantil <wenhao.lin@quantil.com>
2017-01-25 00:37:42 -0800

    implement priority

--- src/views/user/issue-page.js
+++ src/views/user/issue-page.js
@@ -1,9 +1,20 @@
 var angular=require("angular");
-var issue=angular.module("issue",[]);
+var issue=angular.module("issue",['ui.select', require('angular-sanitize')]);
 
 issue.controller("issueController",["$scope", function ($scope) {
     $scope.priority="normal";
+    $scope.priorities=[{
+        id:"0",
+        title:"normal"
+    }, {
+        id:"1",
+        title:"high"
+    }, {
+        id:"2",
+        title:"urgent"
+    }];
 
+    $scope.priority="0";
 
 
 
