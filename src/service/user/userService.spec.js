a4d5faa9503b0de675a6c4d8cbd3c6a63e96ade7
wenhaoquantil <wenhao.lin@quantil.com>
2017-02-07 00:42:23 -0800

    add test case for userService

--- src/service/user/userService.spec.js
+++ src/service/user/userService.spec.js
@@ -3,41 +3,44 @@ var assert=require("chai").assert;
 
 var userService=require("./userService");
 
-describe("test userService", function(done) {
-    console.log("hello");
+describe("test userService", function() {
+    this.timeout(15000);
 
-    beforeEach(function(done) {
-        console.log("before each");
-
+    before(function(done) {
         models.sequelize.sync({force:true}).then(function() {
-
-            seedUser().then(function(results) {
-                console.log(results);
+            seedUser().then(function() {
                 done();
             }, function(error) {
-                console.log(error);
                 done(error);
-            })
+            });
 
         }, function(error) {
             done(error);
         })
+
     });
 
 
-    describe("test searUserByEmail --- found", function(done) {
+    it("test searUserByEmail --- found", function(done) {
         userService.searUserByEmail("wenhao.lin@gmail.com").then(function(user) {
-            console.log(user);
-
-            done(null);
+            assert.equal("wenhao.lin@gmail.com", user.email);
+            done();
         }, function(error) {
             done(error);
         })
+
     });
 
 
+    it("test searUserByEmail --- not exist", function(done) {
+        userService.searUserByEmail("notexist@gmail.com").then(function(user) {
+            assert.equal(null, user);
+            done();
+        }, function(error) {
+            done(error);
+        })
 
-
+    });
 
     function seedUser() {
         var User=models.User;
