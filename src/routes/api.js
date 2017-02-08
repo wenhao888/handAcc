f91509be078e1e34f54343ce497316813b294538
wenhaoquantil <wenhao.lin@quantil.com>
2017-02-07 23:19:44 -0800

    save before testing

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var stringHelp = require("../service/help/stringHelp");
 var userService = require("../service/user/userService");
+var logger= require("../service/logging/logService").getLogger("api");
 
 var router = express.Router();
 
 router.post("/users/email/_search", function(req, res, next) {
     var email = req.body.query.email;
-    var user = userService.getUserByEmail(email) ;
-    res.end(JSON.stringify(user));
+    userService.getUserByEmail(email).then(function(user) {
+        res.end(JSON.stringify(user||{}));
+
+    }, function (error) {
+        logger.error(error);
+        next(error);
+    });
+
 });
 
 module.exports = router;
