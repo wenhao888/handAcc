f4c317bbf8aa06eafbf75183a2622455fc5e3294
wenhaoquantil <wenhao.lin@quantil.com>
2017-02-08 00:16:07 -0800

    implement registration page

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var emailService = require("../service/email/emailService");
 var productService = require("../service/product/productService");
 var router = express.Router();
+
 
 router.get('/login', function(req, res, next) {
   res.render('user/login', {layout:'layout/general'});
@@ -11,5 +11,13 @@ router.get("/registration", function(req, res, next) {
   res.render('user/registration', {layout:'layout/general'});
 });
 
+router.post("/registration", function(req, res, next) {
+  res.redirect("registration-confirm");
+});
+
+
+router.get("/registration-confirm", function(req, res, next) {
+  res.render("user/registration-confirm",{layout:'layout/general'});
+});
 
 module.exports = router;
