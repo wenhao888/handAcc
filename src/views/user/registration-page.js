19f239ae88dd1beaf70f44e3797fb514c9bbf7a7
wenhaoquantil <wenhao.lin@quantil.com>
2017-02-07 23:19:44 -0800

    save before testing

--- src/views/user/registration-page.js
+++ src/views/user/registration-page.js
@@ -3,11 +3,17 @@ require("../shared/validators");
 
 var reg= angular.module("registration",[require('angular-resource'),"validators","ngPasswordStrength"]);
 
-reg.factory("query",["$resource", function ($resource) {
-    return $resource("/api/users/email/_search")
+reg.factory("emailQuery",["$resource", function ($resource) {
+    return $resource("/api/users/email/_search",{}, {
+        search: {
+            method:'post',
+            array:false
+        }
+    })
 }]);
 
-reg.controller("registrationController",["$scope", "query", function ($scope, query) {
+reg.controller("registrationController",["$scope", "emailQuery", function ($scope) {
+
     $scope.cancel = function () {
         window.history.back();
     };
@@ -25,5 +31,28 @@ reg.controller("registrationController",["$scope", "query", function ($scope, qu
        return "strength: " + value;
     }
 
+}]);
+
+
+reg.directive("noneDupEmailValidator",["emailQuery","$q",function(emailQuery, $q) {
+    return {
+        restrict : "C",
+        require: 'ngModel',
+        link: function(scope, elem, attr, ngModel) {
+            ngModel.$asyncValidators.dupEmail = function (modelValue, viewValue) {
+                return $q(function (resolve, reject) {
+                    emailQuery.search({}, {query: {email: modelValue || viewValue}}, function(user) {
+                        if (user.id) {
+                            reject();
+                        } else {
+                            resolve();
+                        }
+                      }, function() {
+                        reject();
+                    })
+                });
+            };
+        }
+    }
 }]);
 
