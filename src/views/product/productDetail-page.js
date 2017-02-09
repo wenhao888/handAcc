d7ecc8b4846fb6bce4f6730022a02282cdf1d8f8
wenhaoquantil <wenhao.lin@quantil.com>
2017-02-09 10:24:35 -0800

    save before testing

--- src/views/product/productDetail-page.js
+++ src/views/product/productDetail-page.js
@@ -1,5 +1,5 @@
 var angular=require("angular");
-var productDetail = angular.module("productDetail",['etalag']);
+var productDetail = angular.module("productDetail",[]);
 
 productDetail.controller("detailController",["$scope", function($scope) {
     $scope.currentIndex=0;
@@ -7,3 +7,34 @@ productDetail.controller("detailController",["$scope", function($scope) {
         $scope.currentIndex = index;
     }
 }]);
+
+productDetail.directive('etalagDisplay', function ($timeout) {
+    return {
+        restrict:'ACE',
+        link:function (scope, element, attrs, controller) {
+            console.log("directive");
+
+            $timeout(function () {
+                var option= {
+                    cursor: 'pointer',
+                    imageCrossfade: true,
+                    zoomWindowHeight: 550,
+                    zoomWindowWidth: 702,
+                    lensSize: 10,
+                };
+
+                $("#zoom").elevateZoom(option);
+
+                $(".gallery-img").mouseenter(function(e){
+                    e.preventDefault();
+                    var dataZoomImg = $(this).attr('data-zoom-image');
+                    var dataImg = $(this).attr('data-image');
+                    var mainImg = $('#zoom');
+                    mainImg.attr('src',dataImg);
+                    mainImg.data('zoom-image',dataZoomImg).elevateZoom(option);
+
+                });
+            })
+        }
+    }
+});
